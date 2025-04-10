8d267888be779d92cbf159a08feda7ed9e9ef18e
Ravula-Swetha <swetharavula1202@gmail.com>
2025-04-10 22:26:49 +0530

    React commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Props from './Components/Props';
+import Counter from './Components/Counter';
+import ClassCounter from './Components/ClassCounter';
+import DestructindProps from './Components/DestructingProps';
+import ObjectDestructure from './Components/ObjectDestructure';
+import DestructingArrays from './Components/DestructingArrays';
+
+// function add(a, b){
+//   return a+b
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {/* <h1>Internal Sum function: {add(2, 3)}</h1>
+       <h1>External Sum funtion: {Addition(10,7)}</h1>   */}
+        <Props name="Jane" college="KMIT"></Props>
+       <Counter/>
+       <ClassCounter /> 
+       <DestructindProps name="Doe" place="Hyderabad"></DestructindProps>
+       <ObjectDestructure></ObjectDestructure>
+       <DestructingArrays></DestructingArrays>
     </div>
   );
 }
---
+++ src/in.js
@@ -0,0 +1,11 @@
+// const react = require("react");
+// const reactDom = require("react-dom");
+
+// let ele = react.createElement("h1", {className: "bg-primary"}, "Hey");
+// reactDom.render(ele, document.getElementById("root"));
+
+function Addition(a, b){
+    return a+b;
+}
+
+export default Addition
